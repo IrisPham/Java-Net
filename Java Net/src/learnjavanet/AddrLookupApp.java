5e8c5598cfa4c695b69abf6388384fe95f5d8b0b
IrisPham <phamhoaianctu@gmail.com>
2017-08-24 23:17:39 +0700

    Khởi tạo tài liệu

---
+++ Java Net/src/learnjavanet/AddrLookupApp.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learnjavanet;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author Visual Studio
+ */
+public class AddrLookupApp {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            //Lấy đối tượng InetAddress chứa thông tin máy chủ.
+            InetAddress host = InetAddress.getByName("www.google.com");
+            //Lấy tên Server nếu có
+            String hostName = host.getHostName();
+            //In các thông tin ra màn hình
+            System.out.println("Host Name: " +hostName);
+            System.out.println("IP address: " + host.getHostAddress());
+        } catch (UnknownHostException e) {
+            System.out.println("Address not found: " + e.getMessage());
+        }
+    }
+}
